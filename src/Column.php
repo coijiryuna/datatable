808c285219b7379a329518775db9186c5316f98f
coijiryuna <newcomputer300@gmail.com>
2025-07-02 13:18:51 +0700

    new file:   .gitignore 	new file:   LICENSE 	new file:   README.md 	new file:   composer.json 	new file:   src/Column.php 	new file:   src/DataTable.php 	new file:   src/DataTableColumnDefs.php 	new file:   src/DataTableQuery.php 	new file:   src/Helper.php 	new file:   src/Request.php

---
+++ src/Column.php
@@ -0,0 +1,14 @@
+<?php 
+namespace Rifa\DataTables;
+
+class Column{
+
+	public $key;
+	public $alias;
+	public $type = 'column';
+	public $callback;
+	public $searchable = TRUE;
+	public $orderable = TRUE;
+
+}
+
